5e75c0c4250c30727b18d5248f0eb27527b1c428
DrewAPicture <drew@10up.com>
2014-05-31 12:03:39 -0600

    Add basic wp_remote_post() operation instead of leveraging a lib for such a simple POST. Remove the HipChat lib.

---
+++ hipchat-webhook.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: Hipchat Webhook
+ * Description: ~
+ * Author: Drew Jaynes
+ * License: GPLv2
+ * Version: 0.1
+ */
+
+/**
+ * Load $api_key from elsewhere.
+ */
+require_once( 'secrets.php' );
+
+$args = array(
+	'method' => 'POST',
+	'body'   => array(
+		'room_id'        => 602229,
+		'from'           => 'VIP/master',
+		'message'        => 'Somebody just pushed code to VIP for review',
+		'notify'         => 0,
+		'color'          => 'purple',
+		'message_format' => 'html',
+		'format'         => 'json',
+		'auth_token'     => $api_key
+	)
+);
+$resp = wp_remote_post( 'https://api.hipchat.com/v1/rooms/message', $args );
+
+if ( '200' == wp_remote_retrieve_response_code( $resp ) ) {
+	error_log( 'success' );
+}
